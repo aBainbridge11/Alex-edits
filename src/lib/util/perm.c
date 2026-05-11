5267eec3eb73f3f46536a6eba9331ed4c5e0f69b
Alexander Bainbridge-Sedivy <alex.bainbridge@inkbridge.io>
2026-05-11 10:25:50 -0400

    fix(perm): wrong sticky-bit mask

--- src/lib/util/perm.c
+++ src/lib/util/perm.c
@@ -42,7 +42,7 @@ char const *fr_perm_mode_to_str(char out[static 10], mode_t mode)
 	strcpy(&out[6], rwx[(mode & 7)]);
 	if (mode & S_ISUID) out[2] = (mode & 0100) ? 's' : 'S';
 	if (mode & S_ISGID) out[5] = (mode & 0010) ? 's' : 'l';
-	if (mode & S_ISVTX) out[8] = (mode & S_IXOTH) ? 't' : 'T';
+	if (mode & S_ISVTX) out[8] = (mode & 0001) ? 't' : 'T';
 	out[9] = '\0';
 
 	return out;
