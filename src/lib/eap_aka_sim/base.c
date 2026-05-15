59c267680252e9ac87adb17cfbd22057afa56ced
Alexander Bainbridge-Sedivy <alex.bainbridge@inkbridge.io>
2026-05-15 10:02:16 -0400

    eap_aka_sim/base: fr_aka_sim_init() leaks loaded dictionaries on enum-autoload and openssl-init error paths

--- src/lib/eap_aka_sim/base.c
+++ src/lib/eap_aka_sim/base.c
@@ -299,11 +299,13 @@ int fr_aka_sim_init(void)
 	}
 	if (fr_dict_enum_autoload(libfreeradius_aka_sim_dict_enum) < 0) {
 		PERROR("Failed loading libfreeradius-eap-aka-sim enumerations");
+		fr_dict_autofree(libfreeradius_aka_sim_dict);
 		return -1;
 	}
 
 	if (fr_openssl_init() < 0) {
 		PERROR("Failed setting up OpenSSL");
+		fr_dict_autofree(libfreeradius_aka_sim_dict);
 		return -1;
 	}
 
