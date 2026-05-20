2e65bba3d376e03564b8bde293dd197afba70090
Alexander Bainbridge-Sedivy <alex.bainbridge@inkbridge.io>
2026-05-20 15:48:22 -0400

    strerror.c: NULL error pointer passed to %s in fr_perror_to_str else-branch

--- src/lib/util/strerror.c
+++ src/lib/util/strerror.c
@@ -795,7 +795,7 @@ char const *fr_perror_to_str(char const *line_sep, char const *fmt, ...)
 	if (error) {
 		if (fr_sbuff_in_sprintf(agg, "%s: %s%s", prefix, error, line_sep) < 0) return NULL;
 	} else {
-		if (fr_sbuff_in_sprintf(agg, "%s%s", prefix, error, line_sep) < 0) return NULL;
+		if (fr_sbuff_in_sprintf(agg, "%s%s", prefix, line_sep) < 0) return NULL;
 		talloc_free(prefix);
 		return NULL;
 	}
