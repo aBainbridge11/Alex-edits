a9671fe11e896da7a9482616a454efb78f382012
Alexander Bainbridge-Sedivy <alex.bainbridge@inkbridge.io>
2026-06-05 12:18:32 -0400

    curl/xlat: unchecked return values of fr_value_box_strdup and fr_value_box_bstrndup on error path

--- src/lib/curl/xlat.c
+++ src/lib/curl/xlat.c
@@ -58,7 +58,11 @@ xlat_action_t fr_curl_xlat_uri_escape(UNUSED TALLOC_CTX *ctx, fr_dcursor_t *out,
 	if (strlen(escaped) == to_escape->vb_length) goto done;
 
 	fr_value_box_clear_value(to_escape);
-	fr_value_box_strdup(to_escape, to_escape, NULL, escaped, to_escape->tainted);
+	if (fr_value_box_strdup(to_escape, to_escape, NULL, escaped, to_escape->tainted) < 0) {
+		curl_free(escaped);
+		talloc_free(to_escape);
+		return XLAT_ACTION_FAIL;
+	}
 
 done:
 	curl_free(escaped);
@@ -94,7 +98,11 @@ xlat_action_t fr_curl_xlat_uri_unescape(UNUSED TALLOC_CTX *ctx, fr_dcursor_t *ou
 	}
 
 	fr_value_box_clear_value(to_unescape);
-	fr_value_box_bstrndup(to_unescape, to_unescape, NULL, unescaped, unescaped_len, to_unescape->tainted);
+	if (fr_value_box_bstrndup(to_unescape, to_unescape, NULL, unescaped, unescaped_len, to_unescape->tainted) < 0) {
+		curl_free(unescaped);
+		talloc_free(to_unescape);
+		return XLAT_ACTION_FAIL;
+	}
 	curl_free(unescaped);
 	fr_dcursor_insert(out, to_unescape);
 
