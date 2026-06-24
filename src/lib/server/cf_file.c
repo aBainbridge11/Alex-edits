87cb965c1cbe5a8d184336e09c80a62cbec4aa01
Alexander Bainbridge-Sedivy <alex.bainbridge@inkbridge.io>
2026-06-24 15:53:37 -0400

    lib/server/cf_file: OOB read in process_include() when $INCLUDE target expands to empty string

--- src/lib/server/cf_file.c
+++ src/lib/server/cf_file.c
@@ -988,6 +988,11 @@ int cf_section_pass2(CONF_SECTION *cs)
 					    (cp->rhs_quote != T_BARE_WORD));
 		if (!value) return -1;
 
+		if (!*value) {
+			ERROR("%s[%d]: Empty filename in $INCLUDE", frame->filename, frame->lineno);
+			return -1;
+		}
+
 		talloc_const_free(cp->value);
 		cp->value = talloc_strdup(cp, value);
 	}
